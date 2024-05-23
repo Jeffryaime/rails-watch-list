9c3bca72e141701057fe716e1012d7fe7ddff5ce
jeff prince <jeffryaime@gmail.com>
2024-05-23 16:37:40 -0400

    Movie List

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,33 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list  # Ensure the bookmark is associated with the list
+    if @bookmark.save
+      redirect_to list_path(@list), notice: 'Bookmark was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), notice: 'Bookmark was successfully deleted.'
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])  # Find the list from the URL parameter
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
 end
